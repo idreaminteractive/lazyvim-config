e0487373e7e4711773f73f2c4ce29d2233db518f
Dave Wiper <dwiper@idreaminteractive.com>
2025-08-29 02:23:09 +0000

    chore: iterating...

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        -- set to 'none' to disable the 'default' preset
+        preset = "default",
+
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+      },
+    },
+  },
+}
