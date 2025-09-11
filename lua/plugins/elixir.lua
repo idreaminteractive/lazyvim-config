b3a7ea5dc074b9143fa19c983c43e040b396ea66
Dave Wiper <dwiper@idreaminteractive.com>
2025-09-11 17:50:53 +0000

    chore: iterating...

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -1,17 +1,18 @@
-vim.lsp.config("expert", {
-  cmd = { "expert" },
-  root_markers = { "mix.exs", ".git" },
-  filetypes = { "elixir", "eelixir", "heex" },
-})
-
-vim.lsp.enable("expert")
+-- vim.lsp.config("expert", {
+--   cmd = { "expert" },
+--   root_markers = { "mix.exs", ".git" },
+--   filetypes = { "elixir", "eelixir", "heex" },
+-- })
+--
+-- vim.lsp.enable("expert")
+-- print out the lsp config
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         -- Or, if you're using the standard LSP setup, disable it here:
-        elixirls = false,
+        -- elixirls = false,
       },
     },
   },
